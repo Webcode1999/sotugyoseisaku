c976a4998d38106d0726cfe877540a70d23ad0f9
DOUBLALI AMINE <94901238+Aminesam1092@users.noreply.github.com>
2022-11-30 16:04:43 +0900

    first commit

---
+++ js/style.js
@@ -0,0 +1,15 @@
+//スクロールトリガーコード//
+const container = document.querySelector("#scroll-container");
+const height = container.clientHeight;
+document.body.style.height = `${height}px`;
+
+gsap.to(container, {
+  y: -(height - document.documentElement.clientHeight),
+  ease: "none",
+  scrollTrigger: {
+    trigger: document.body,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 3,
+  },
+});
