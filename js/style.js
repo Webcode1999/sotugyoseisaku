8ad120724b07b8684796134dadf59f821007a761
DOUBLALI AMINE <94901238+Aminesam1092@users.noreply.github.com>
2022-12-08 13:11:13 +0900

    first commit

--- js/style.js
+++ js/style.js
@@ -1,5 +1,4 @@
 const targets = document.querySelectorAll(".Topimageconteiner");
-
 targets.forEach((target) => {
   gsap.fromTo(
     target.querySelector("img"),
@@ -31,12 +30,40 @@ gsap.to(".Moroccanfoods_headtext", {
   opacity: 1,
 });
 
-gsap.to(".Moroccanfoods_taginebox", {
+gsap.to(".jsanimation", {
   scrollTrigger: {
-    trigger: ".Moroccanfoods_conteiner",
+    trigger: ".jsanimation",
     start: "top center",
     end: "bottom center",
   },
-  duraion: 5,
+  delay: 0.2,
   opacity: 1,
 });
+
+gsap.to(".jsanimationsecond", {
+  scrollTrigger: {
+    trigger: ".jsanimationsecond",
+    start: "top center",
+    end: "bottom center",
+  },
+  delay: 0.3,
+  opacity: 1,
+});
+
+const target = document.querySelector(".Moroccanfoods_centrarimage");
+gsap.fromTo(
+  target,
+  {
+    y: 0,
+  },
+  {
+    y: -60,
+    scrollTrigger: {
+      trigger: target, // ScrollTriggerの開始位置を計算するために使用される要素
+      start: "top bottom", // 1つ目の値がtriggerで指定した要素の開始位置　2つ目の値が画面の開始位置
+      end: "bottom top", // 1つ目の値がtriggerで指定した要素の終了位置　2つ目の値が画面の終了位置
+      scrub: 1, // 要素を1秒遅れで追従させる
+      // markers: true, // 開始位置、終了位置を調整確認する際に使用します
+    },
+  }
+);
